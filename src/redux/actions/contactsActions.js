0a09a488e73482153f24141e5bb8cc5ed6f13288
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-11-19 19:24:34 +0200

    dgfhg

--- src/redux/actions/contactsActions.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const filter = createAction('items/filter');
-export const setIsLoading = createAction('isLoading/setIsLoading');
-export const setAuthStatus = createAction('authStatus/setAuthStatus');
